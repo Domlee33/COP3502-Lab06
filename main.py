56a9c91f38d2b2b4d45502324d70fbedacdcf6ce
joey <jmackle@ufl.edu>
2023-07-19 19:45:40 -0400

    decode function added, works within main loop

--- main.py
+++ main.py
@@ -11,27 +11,29 @@ def encode(nums):
             enco += str(i - 7)
     return enco
 
-def decode():
-    pass
-
+def decode(password):  # decode function coded by Joseph Mackle
+    decoded = ""
+    for i in range(len(password)):
+        j = int(password[i]) - 3
+        if j < 0: j += 10  # account for numbers going negative
+        decoded += str(j)
+    return decoded
 
 if __name__ == '__main__':
-
+    ans = ""  # initialize variable here so it can be accessed in later loops
     while True:
         print('Menu\n-------------')
         print('1. Encode')
         print('2. Decode')
-        print('3. Quit')
+        print('3. Quit\n')
         choice = int(input('Please enter an option: '))
 
         if choice == 1:
             code = input('Please enter your password to encode: ')
             ans = encode(code)
             print('Your password has been encoded and stored!')
-
         if choice == 2:
-            pass
-
+            print(f'The encoded password is {ans}, and the original password is {decode(ans)}.')
         if choice == 3:
             break
 
